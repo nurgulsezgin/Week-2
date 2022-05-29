18e50ae1f7232cece20d9d5ac8821098b5259d91
nurgül sezgin <nurgul_sezgin@hotmail.com>
2022-05-29 21:05:18 +0300

    Initial Commit

---
+++ ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by Nurgul on 29.05.2022.
+//  Copyright © 2022 Nurgul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
